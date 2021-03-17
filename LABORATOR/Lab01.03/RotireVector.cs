bdf084f1b8bd94dcd28b7a60c7668cc2389f2876
Vasile Orbai <vasile930@gmail.com>
2021-03-17 19:20:26 +0200

    Add lab1

---
+++ LABORATOR/Lab01.03/RotireVector.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01._03
+{
+    class RotireVector
+    {
+        static void Main(string[] args)
+        {
+            //Rotirea elementelor unui vector
+
+            Console.Write("Dati numarul de elemente al vectorului: ");
+            int lungimeVector = int.Parse(Console.ReadLine());
+            int[] vector = new int[lungimeVector];
+            for (int i = 0; i < lungimeVector; i++)
+            {
+                vector[i] = i + 1;
+            }
+            Console.Write("Vectorul initial este: ");
+            afisareVector(vector);
+            Console.Write("Dati numarul de rotatii: ");
+            int nrRotatii = int.Parse(Console.ReadLine());
+            int[] vectorFinal = VectorRotit(vector,nrRotatii);
+            Console.Write("Vectorul final este: ");
+            afisareVector(vectorFinal);
+        }
+        static void afisareVector(int[] vector)
+        {
+            int lungimeVector = vector.Length;
+            for (int i = 0; i < lungimeVector; i++)
+            {
+                Console.Write($"{vector[i]} ");
+            }
+            Console.WriteLine();
+        }
+        static int[] VectorRotit(int [] vector, int nrRotatii)
+        {
+            if (nrRotatii==0)
+            {
+                return vector;
+            }
+            int[] aux = new int[vector.Length];
+            int j = 0;
+            for (int i = nrRotatii; i < vector.Length; i++)
+            {
+                aux[j] = vector[i];
+                j++;
+            }
+            for (int i = 0; i < nrRotatii; i++)
+            {
+                aux[j] = vector[i];
+                j++;
+            }
+            return aux;
+        }
+    }
+}
---
+++ LABORATOR/Lab01.03/SumaRecursiv.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01._03
+{
+    class SumaRecursiv
+    {
+        static void Main(string[] args)
+        {
+            //Suma elementelor unui vector
+            int[] vector = new int[] { 1, 2, 3, 4, 5,};
+            //int rezultat = sumaBasic(vector);
+            int rezultat = sumaRecursiv(vector,0);
+            Console.Write("Elementele vectorului sunt: ");
+            afisareVector(vector);
+            Console.Write("Suma elementelor vectorului este: ");
+            Console.WriteLine(rezultat);
+        }
+        static int sumaBasic(int[] vector)
+        {
+            int suma = 0;
+            for (int i = 0; i < vector.Length; i++)
+            {
+                suma += vector[i];
+            }
+            return suma;
+        }
+        static void afisareVector(int[] vector)
+        {
+            int lungimeVector = vector.Length;
+            for (int i = 0; i < lungimeVector; i++)
+            {
+                Console.Write($"{vector[i]} ");
+            }
+            Console.WriteLine();
+        }
+        static int sumaRecursiv(int[] vector, int index)
+        {
+            if (index==vector.Length)
+            {
+                return 0;
+            }
+            return vector[index] + sumaRecursiv(vector, index + 1);
+        }
+    }
+}
